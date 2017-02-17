8a3e58c51a0311edd1d44428b599b851c51ada95
thiagolucio <thiagolucio@gmail.com>
2017-02-17 17:26:12 -0200

    primeiro push do jogo

---
+++ trabalhando-string.js
@@ -0,0 +1,8 @@
+let nome = 'Primeiro';
+let snome = 'Segundo';
+let completo = nome + snome;
+document.write(completo.length + '<br>'); //pular linha usando html dentro do js
+document.write(completo.charAt(3) + '<br>'); // mostrar a letra da posição 3 da variavel completo
+document.write(completo.substring(0,4) + '<br>'); //mostrar a variavel completo da primeira posicao até a quarta
+document.write(completo.toLowerCase() + '<br>');//converter a string da variavel completo em letras minusculas
+document.write(completo.toUpperCase() + '<br>');//converter a string da variavel completo em letras maiúsculas
